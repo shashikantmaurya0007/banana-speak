a3bb8b466778bb890312178675b84c25b5b28715
shashikantmaurya0007 <80745766+shashikantmaurya0007@users.noreply.github.com>
2021-06-01 00:40:09 +0530

    next step

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
-// var value=document.querySelector("#first-button");
+var btntranslate=document.querySelector('#first-button');
 
 function eventhandler()
 {
      console.log("clicked");
 }
+
+btntranslate.addEventListener("click",eventhandler);
