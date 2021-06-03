6c1d42ee34b664423aa82b2b4c123a42a7692389
shashikantmaurya0007 <80745766+shashikantmaurya0007@users.noreply.github.com>
2021-06-03 17:48:19 +0530

    fetch working

--- app.js
+++ app.js
@@ -1,8 +1,24 @@
-var btntranslate=document.querySelector('#first-button');
 
-function eventhandler()
-{
-     console.log("clicked");
+var buttontranslate = document.querySelector("#btn-translate");
+var textinput = document.querySelector("#text-input");
+var output = document.querySelector("#output-area");
+function errorhandler() {
+  alert("something wrong with server please try later");
+}
+function eventhandler() {
+  var s = textinput.value;
+  fetch(
+    "https://api.funtranslations.com/translate/ferb-latin.json" +
+      "?" +
+      "text=" +
+      s
+  )
+    .then((response) => response.json())
+    .then((json) => {
+      var translated = json.contents.translated;
+      output.innerText = translated;
+    })
+    .catch(errorhandler);
 }
 
-btntranslate.addEventListener("click",eventhandler);
+buttontranslate.addEventListener("click", eventhandler);
